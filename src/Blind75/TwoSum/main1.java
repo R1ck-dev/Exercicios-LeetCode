f4e94467badab7a20f09dc0c17f7991f69f91539
Henrique Marangoni <henrique.1108@hotmail.com>
2025-08-29 13:32:28 -0300

    Two Sum

---
+++ src/Blind75/TwoSum/main1.java
@@ -0,0 +1,10 @@
+package Blind75.TwoSum;
+
+import java.util.Arrays;
+
+public class main1 {
+    public static void main(String[] args) {
+        int[] resultado = TwoSum.twoSum(new int[]{4,5,6}, 10);
+        System.out.println(Arrays.toString(resultado));
+    }
+}
