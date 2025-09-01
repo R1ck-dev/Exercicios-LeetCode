01327ce6219f659f240396688096b6f1ce4535c4
Henrique Marangoni <henrique.1108@hotmail.com>
2025-08-31 22:39:45 -0300

    feats: Group Anagrams

--- src/Blind75/GroupAnagrams/GroupAnagrams.java
+++ src/Blind75/GroupAnagrams/GroupAnagrams.java
@@ -1,49 +1,20 @@
 package Blind75.GroupAnagrams;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-// import java.util.Collections;
-import java.util.List;
-// import java.util.stream.Stream;
+import java.util.*;
 
 public class GroupAnagrams {
     public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
 
-        // List<String> strs lengthArrayList<>(Arrays.asList(strs));
-        List<List<String>> strsReturnList = new ArrayList<>();
-        List<String> insideList = new ArrayList<>();
-        int control = 0;
+        for (String s : strs) {
 
-        for (int i = 0; i < strs.length; i++) {
-            for (int j = 0; j < strs.length; j++) {
-                if (i == j) {
-                    continue;
-                }
-                char[] string1 = strs[i].toCharArray();
-                char[] string2 = strs[j].toCharArray();
-                Arrays.sort(string1);
-                Arrays.sort(string2);
-                if (Arrays.equals(string1, string2)) {
-                    if (strsReturnList.get(i).contains(strs[i])) {
-                        insideList.add(strs[j]);
-                        strsReturnList.add(control, new ArrayList<>(insideList));
-                        insideList.clear();
-                    } else {
-                        insideList.add(strs[i]);
-                        insideList.add(strs[j]);
-                        strsReturnList.add(control, new ArrayList<>(insideList));
-                        insideList.clear();
-                    }
-
-                }
-            }
-            if (!(strsReturnList.get(i).contains(strs[i]))) {
-                insideList.add(control, strs[i]);
-                strsReturnList.add(new ArrayList<>(insideList));
-                insideList.clear();
-            }
-            control++;
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+            
         }
-        return strsReturnList;
+
+        return new ArrayList<>(map.values());
     }
 }
